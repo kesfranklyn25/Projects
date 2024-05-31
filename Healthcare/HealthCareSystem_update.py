b5290fc270aba6f3b04229bd7ce89899db11b904
kesfranklyn25 <129005089+kesfranklyn25@users.noreply.github.com>
2024-05-31 21:37:52 +0100

    Update HealthCareSystem_update.py

--- Healthcare/HealthCareSystem_update.py
+++ Healthcare/HealthCareSystem_update.py
@@ -42,9 +42,8 @@ st.markdown("""
 <style>
     [data-testid=stSidebar] {
         background-color: #40e0d0;
-        .menu.nav-item.nav-link.active[data-v-5af006b8] {
-            background-color: #40e0d0;
-        }
+        .menu .nav-item .nav-link.active[data-v-5af006b8] {
+    background-color: #40e0d0(--primary-color);
         background-color: ;
         }
 </style>
