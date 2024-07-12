f076f79bf50549403ad8e90dbc7f476db3d243e5
kesfranklyn25 <129005089+kesfranklyn25@users.noreply.github.com>
2024-07-12 17:03:51 +0100

    Update HealthCare_System_RF_Implementation.py
    Update with BMI computation submenu

--- HealthCare_System_RF_Implementation.py
+++ HealthCare_System_RF_Implementation.py
@@ -1,91 +1,112 @@
-# Load required libraries
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  5 16:47:02 2024
 
-# import numpy as np
+@author: KesBes
+"""
+
+# Load required libraries
 import streamlit as st
 import pickle
 from streamlit_option_menu import option_menu
 from PIL import Image
-img = Image.open('salford.png')
-# st.beta
 
+# Load the image
+img = Image.open('salford.png')
 
 # Set the title page of the web app page
 st.set_page_config(
-    page_title= 'Healthcare System - Diabetes Prediction',
-    page_icon= img
-    
-    )
+    page_title='Healthcare System - Diabetes Diagnosis',
+    page_icon=img
+)
+
 # Display the logo at the top of the page
 st.image(img, width=150)
-# Title page ends here
+
 # Load models
-rf = pickle.load(open('rf.sav','rb'))
-# Models loading ends here.
+rf = pickle.load(open('rf.sav', 'rb'))
 
-
-
-# Hidden the visibilty of the deploy/share and the settings menu on the top right 
+# Hide the visibility of the deploy/share and the settings menu on the top right
 # corner of the web app page
 st.markdown("""
-            <style>
-                .eyeqlp51.st-emotion-cache-1pbsqtx.ex0cdmw0
-                { visibility: hidden;
-                    }
-                .st-emotion-cache-1wbqy5l{
-                    visibility: hidden;
-                    position: None;}
-            </style>
-            
-            """, unsafe_allow_html=True)
+    <style>
+        .eyeqlp51.st-emotion-cache-1pbsqtx.ex0cdmw0 {
+            visibility: hidden;
+        }
+        .st-emotion-cache-1wbqy5l {
+            visibility: hidden;
+            position: None;
+        }
+    </style>
+    """, unsafe_allow_html=True)
 
-# deploy/share and settings ends here
-
-# set the side menu background color
+# Set the side menu background color
 st.markdown("""
-<style>
-    [data-testid=stSidebar] {
-        background-color: #40e0d0;
-        
-
-    }
-</style>
-""", unsafe_allow_html=True)
-# side menu background color ends here
+    <style>
+        [data-testid=stSidebar] {
+            background-color: #40e0d0;
+        }
+    </style>
+    """, unsafe_allow_html=True)
 
 # Create a sidebar menu for the models
 with st.sidebar:
-    selection = option_menu('Menu', 
-                            ['RF Prediction'],
-                            icons = ['tree-fill'],
-                            default_index = 0)
+    selection = option_menu('Menu',
+                            ['BMI Computation', 'Diabetes Diagnosis'],
+                            icons=['tree-fill'],
+                            default_index=1)
     st.write('Welcome to the Healthcare System - Diabetes Prediction application! \
              This app is designed to provide a quick and efficient way to assess \
              the likelihood of diabetes based on readily available health parameters. \
-                 Our goal is to leverage advanced machine learning techniques to offer \
-                     accurate predictions, helping individuals make informed decisions \
-                         about their health..')
-   
-# sidebar menu creation ends here
+             Our goal is to leverage advanced machine learning techniques to offer \
+             accurate predictions, helping individuals make informed decisions \
+             about their health.')
 
-# Code the different  models
-if selection == 'RF Prediction':
+# Initialize bmi_value
+bmi_value = 0
+
+# BMI Computation
+if selection == 'BMI Computation':
     # Give the page a title
-    # st.image(img, width=150)
+    html_temp = """
+    <div style="background:#40e0d0 ;padding:10px">
+    <h2 style="color:white;text-align:center;">Body Mass Index Computation</h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+    col1, col2 = st.columns(2)
+    # Get user data for prediction
+    with col1:
+        weight = st.number_input('Weight(kg)', min_value=0.0, key='weight')
+    with col2:
+        height = st.number_input('Height(m)', min_value=1.0, key='height')
+    with col2:
+        if st.button('Compute'):
+            bmi_value = round(weight / (height * height), 2)
+    with col1:
+        st.success(bmi_value)
+# Code the different models
+
+else:
+    # Give the page a title
     html_temp = """
     <div style="background:#40e0d0 ;padding:10px">
     <h2 style="color:white;text-align:center;">Diabetes Test </h2>
     </div>
     """
-    st.markdown(html_temp, unsafe_allow_html = True)
+    st.markdown(html_temp, unsafe_allow_html=True)
     col1, col2 = st.columns(2)
     # Get user data for prediction
     with col1:
-        Glucose = st.number_input('Glucose Level', min_value=0, key='Glucose')
-        BloodPressure = st.number_input('Blood Pressure Level', min_value=0, key='BloodPressure')
+        Glucose = st.number_input('Glucose', min_value=0.0, key='Glucose')
+        BloodPressure = st.number_input('Blood Pressure Level *', min_value=0.0, key='BloodPressure')
 
     with col2:
-        BMI = st.number_input('Body Mass Index', min_value=0.0, key='BMI')
-        Age = st.number_input('Patient Age', min_value=12, key='Age')
+        BMI = st.number_input('Body Mass Index *', min_value=0.0, key='BMI')
+        Age = st.number_input('Age *', min_value=16, key='Age')
+
+        if BMI == 0:
+            BMI = bmi_value
 
     prediction = ''
     with col2:
@@ -95,12 +116,11 @@ if selection == 'RF Prediction':
                 prediction = 'DIABETIC'
             else:
                 prediction = 'NOT DIABETIC'
-            
+
     with col1:
         st.success(prediction)
 
-
-#  disclaimer 
+# Disclaimer
 disclaimer_temp = """
 <div style="background:#f9f9f9; padding:10px; margin-top:20px; border-top: 1px solid #ddd;">
 <h4 style="color:#333;text-align:center;">Disclaimer</h4>
