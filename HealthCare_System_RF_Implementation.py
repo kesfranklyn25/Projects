410f7f88bba35d09ab9c61ecce18e5e958cb7420
kesfranklyn25 <129005089+kesfranklyn25@users.noreply.github.com>
2024-07-13 16:24:27 +0100

    Update HealthCare_System_RF_Implementation.py
    Updated version - change of theme + responsiveness

--- HealthCare_System_RF_Implementation.py
+++ HealthCare_System_RF_Implementation.py
@@ -1,191 +1,3 @@
-# # -*- coding: utf-8 -*-
-# """
-# Created on Fri Jul  5 16:47:02 2024
-
-# @author: KesBes
-# """
-
-# # -----------------------------------------Load required libraries------------------------------------------
-# import streamlit as st
-# import pickle
-# from streamlit_option_menu import option_menu
-# from PIL import Image
-# # -----------------------------------------Load required libraries------------------------------------------
-
-# # --------------------------------------------------Load the image-----------------------------------------
-# img = Image.open('AIDScanner_Logo.png')
-# # --------------------------------------------------Load the image-----------------------------------------
-
-# # ----------------------------------Set the title page of the web app page---------------------------------
-# st.set_page_config(
-#     page_title='Healthcare System - Diabetes Diagnosis',
-#     page_icon=img
-# )
-# # ----------------------------------Set the title page of the web app page---------------------------------
-
-# # -----------------------------------------Display the logo at the top of the page-------------------------
-# st.image(img, width=150)
-# # -----------------------------------------Display the logo at the top of the page-------------------------
-
-# # --------------------------------------Load models--------------------------------------------------------
-# rf = pickle.load(open('rf.sav', 'rb')) # Diabetes prediction model
-# gluco_dt = pickle.load(open('gluco_dt.sav', 'rb')) # Glucose estimation/forecast model
-
-# # --------------------------------------Load models--------------------------------------------------------
-
-# # -----------------------------Set the side menu background color of the sidebar---------------------------
-# st.markdown("""
-#     <style>
-#         [data-testid=stSidebar] {
-#             background-color: #13202c;
-#         }
-#     </style>
-#     """, unsafe_allow_html=True)
-# # -----------------------------Set the side menu background color of the sidebar---------------------------
-
-# # ---------------------------Hide the share, deploy and settings of streamlit-----------------------------
-# st.markdown("""
-#     <style>
-#         .eyeqlp51.st-emotion-cache-1pbsqtx.ex0cdmw0 {
-#             # visibility: hidden;
-#             visibility: hidden;
-#         }
-#         .st-emotion-cache-1wbqy5l {
-#             visibility: hidden;}   
-#     </style>
-#     """, unsafe_allow_html=True)
-# # ---------------------------Hide the share, deploy and settings of streamlit-----------------------------
-
-# # ---------------------------sidebar menu for the model Starts Here--------------------------------------
-# with st.sidebar:
-#     selection = option_menu('Menu',
-#                             ['How To Use','Get Glucose Level', 'BMI Calculation', 'Diabetes Diagnosis'],
-#                              default_index=0)
-# # ---------------------------sidebar menu for the model Ends Here----------------------------------------
-# # Initialize bmi_value
-# bmi_value = 0
-# # --------------------------- Default Page- User Manual Starts Here--------------------------------------
-# if selection == 'How To Use':
-#     st.write('Welcome to AIDScanner (Artificial Intelligence Diabetes Scanner) App.\
-#              This web application helps you understand your diabetes status \
-#             through various features. The app consists of four main sections, \
-#             accessible via the menu on the left side of the page:')
-#     lst = ['How to Use:\
-#            This guide explains how to navigate and utilize the app effectively.',
-#            'Get Glucose Level: Use this section to estimate your glucose level \
-#             if you do not know it. Input the required information, and\
-#             the app will compute it.', 'BMI Computation: \
-#             This tool calculates your Body Mass Index (BMI) if you do not know \
-#             it. Enter your height and weight, and the app will compute your BMI\
-#             for you.', 'Diabetes Diagnosis: This is the main feature of the app. \
-#             Here, you can get a diagnosis of your diabetes status based on the \
-#             information you provide. Follow the prompts to input your details \
-#             and receive an assessment.']
-
-#     for i in lst:
-#         st.markdown("- " + i)
-# # --------------------------- Default Page- User Manual Ends Here----------------------------------------           
-
-# # ---------------------------------------Glucose Computation Starts Here---------------------------------
-# elif selection == 'Get Glucose Level':
-#     html_temp = """
-#     <div style="background:#13202c ;padding:10px">
-#     <h2 style="color:white;text-align:center;">Know Your Glucose Level</h2>
-#     </div>
-#     """
-#     st.markdown(html_temp, unsafe_allow_html=True)
-#     col1, col2 = st.columns(2)
-#     # Get user data for glucose estimation
-#     with col1:
-#         # Glucose = st.number_input('Glucose', min_value=0.0, key='Glucose')
-#         BloodPressure = st.number_input('Blood Pressure Level *', min_value=0.0, key='BloodPressure')
-
-#     with col2:
-#         BMI = st.number_input('Body Mass Index *', min_value=0.0, key='BMI')
-#         Age = st.number_input('Age *', min_value=16, key='Age')
-#     glucose_estimate = 0
-#     with col2:
-#         if st.button('Compute'):
-#             glucose_estimate = int(gluco_dt.predict([[BloodPressure, BMI, Age]]))
-#             # prediction = glucose
-
-#     with col1:
-#         st.success(glucose_estimate)
-# # ---------------------------------------Glucose Computation Ends Here-----------------------------------
-
-# # ---------------------------------------BMI Computation Starts Here-------------------------------------
-# elif selection == 'BMI Calculation':
-#     # Give the page a title
-#     html_temp = """
-#     <div style="background:#13202c ;padding:10px">
-#     <h2 style="color:white;text-align:center;">Body Mass Index Computation</h2>
-#     </div>
-#     """
-#     st.markdown(html_temp, unsafe_allow_html=True)
-#     col1, col2 = st.columns(2)
-#     # Get user data for prediction
-#     with col1:
-#         weight = st.number_input('Weight(kg)', min_value=0.0, key='weight')
-#     with col2:
-#         height = st.number_input('Height(m)', min_value=1.0, key='height')
-#     with col2:
-#         if st.button('Compute'):
-#             bmi_value = round(weight / (height * height), 2)
-#     with col1:
-#         st.success(bmi_value)
-# # ---------------------------------------BMI Computation Ends Here------------------------------------
-
-# # ---------------------------------------Classifier Prediction Starts Here----------------------------
-# else:
-#     # Give the page a title
-#     html_temp = """
-#     <div style="background:#13202c ;padding:10px">
-#     <h2 style="color:white;text-align:center;">Welcome to AIDScanner! </h2>
-#     </div>
-#     """
-#     st.markdown(html_temp, unsafe_allow_html=True)
-#     col1, col2 = st.columns(2)
-#     # Get user data for prediction
-#     with col1:
-#         Glucose = st.number_input('Glucose', min_value=0.0, key='Glucose')
-#         BloodPressure = st.number_input('Blood Pressure Level *', min_value=0.0, key='BloodPressure')
-
-#     with col2:
-#         BMI = st.number_input('Body Mass Index *', min_value=0.0, key='BMI')
-#         Age = st.number_input('Age *', min_value=16, key='Age')
-
-#         if BMI == 0:
-#             BMI = bmi_value
-
-#     prediction = ''
-#     with col2:
-#         if st.button('Outcome'):
-#             outcome = rf.predict([[Glucose, BloodPressure, BMI, Age]])
-#             if outcome[0] == 1:
-#                 prediction = 'DIABETIC'
-#             else:
-#                 prediction = 'NOT DIABETIC'
-
-#     with col1:
-#         st.success(prediction)
-# # ---------------------------------------Classifier Prediction Ends Here------------------------------
-# # --------------------------------------------Disclaimer Starts Here----------------------------------
-
-# disclaimer_temp = """
-# <div style="background:#13202c; padding:10px; margin-top:20px; border-top: 1px solid #ddd;">
-# <h4 style="color:#fff;text-align:center;">Disclaimer</h4>
-# <p style="color:#fff;text-align:center;">
-# This application predicts the likelihood of diabetes based on the given parameters.
-# However, it is not a substitute for professional medical advice, diagnosis, or treatment.
-# Always seek the advice of your physician or other qualified health providers with any questions you may have regarding diabetes.
-# </p>
-# </div>
-# """
-# st.markdown(disclaimer_temp, unsafe_allow_html=True)
-
-# # --------------------------------------------Disclaimer Ends Here----------------------------------------
-
-
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jul  5 16:47:02 2024
@@ -200,31 +12,43 @@ from streamlit_option_menu import option_menu
 from PIL import Image
 
 # Load the image
-
 img = Image.open('AIDScanner_Logo.png')
 
-# Set the title page of the web app page
+# ------------------------Page Title--------------------------------------
 st.set_page_config(
     page_title='AIDScanner- ',
     page_icon=img,
     layout="wide"  # Use wide layout for better responsiveness
 )
 
+# ------------------------Page Title--------------------------------------
+
+# --------------------------App Logo--------------------------------------
 # Display the logo at the top of the page
+st.text('')
 st.text('')
 st.text('')
 st.image(img, width=150)
 
-# Load models
-rf = pickle.load(open('rf.sav', 'rb'))
-gluco_dt = pickle.load(open('gluco_dt.sav', 'rb'))
+# --------------------------App Logo--------------------------------------
 
-# Set the side menu background color
+# --------------------------Load Libraries--------------------------------
+rf = pickle.load(open('rf.sav', 'rb'))
+gluco_dt = pickle.load(open('gluco_dt.sav',\
+                            'rb'))
+
+# --------------------------Load Libraries--------------------------------
+
+# --------------------------Sidebar Colour--------------------------------
 st.markdown("""
     <style>
         [data-testid=stSidebar] {
-            background-color: #13202c;
+            background-color: #000080;
         }
+        # .menu .nav-item .nav-link.active[data-v-5af006b8] {
+        #     background-color: #000080;
+        # }
+ 
         .st-emotion-cache-1wbqy5l {
             visibility: hidden;
         }
@@ -236,41 +60,54 @@ st.markdown("""
         }
     </style>
     """, unsafe_allow_html=True)
+    
+# --------------------------Sidebar Colour--------------------------------
 
-# Create a sidebar menu for the models
+# --------------------------Sidebar Menu----------------------------------
 with st.sidebar:
     selection = option_menu('Menu',
-                            ['How To Use','Get Glucose Level', 'BMI Calculation', 'Diabetes Diagnosis'],
+                            ['How To Use','Get Glucose Level', \
+                             'BMI Calculation', 'Diabetes Diagnosis'],
                              default_index=0)
+        
+# --------------------------Sidebar Menu----------------------------------
 
-# Initialize bmi_value
+# --------------------------Default Page----------------------------------
 bmi_value = 0
-
 if selection == 'How To Use':
     st.write("""
     ##### Welcome to AIDScanner (Artificial Intelligence Diabetes Scanner) App
-    This web application helps you understand your diabetes status through various features. 
-    The app consists of four main sections, accessible via the menu on the left side of the page:
-    """)
+    This web application helps you understand your diabetes status through \
+        various features. The app consists of four main sections, accessible \
+        via the menu on the left side of the page:""")
     lst = [
-        'How to Use: This guide explains how to navigate and utilize the app effectively.',
-        'Get Glucose Level: Use this section to estimate your glucose level if you do not know it. Input the required information, and the app will compute it.',
-        'BMI Computation: This tool calculates your Body Mass Index (BMI) if you do not know it. Enter your height and weight, and the app will compute your BMI for you.',
-        'Diabetes Diagnosis: This is the main feature of the app. Here, you can get a diagnosis of your diabetes status based on the information you provide. Follow the prompts to input your details and receive an assessment.'
+        'How to Use: This guide explains how to navigate and utilize the app \
+        effectively.', 'Get Glucose Level: Use this section to estimate your \
+        glucose level if you do not know it. Input the required information, \
+            and the app will compute it.',
+        'BMI Computation: This tool calculates your Body Mass Index (BMI) if \
+        you do not know it. Enter your height and weight, and the app will \
+        compute your BMI for you.', 'Diabetes Diagnosis: This is the main \
+        feature of the app. Here, you can get a diagnosis of your diabetes \
+        status based on the information you provide. Follow the prompts\
+        to input your details and receive an assessment.'
     ]
     for i in lst:
         st.markdown("- " + i)
 
-# Glucose Computation
+# --------------------------Default Page----------------------------------
+
+# --------------------------Glucose Page----------------------------------
 elif selection == 'Get Glucose Level':
     st.markdown("""
-    <div style="background:#13202c ;padding:10px">
+    <div style="background:#000080 ;padding:10px">
     <h2 style="color:white;text-align:center;">Know Your Glucose Level</h2>
     </div>
     """, unsafe_allow_html=True)
     col1, col2 = st.columns(2)
     with col1:
-        BloodPressure = st.number_input('Blood Pressure Level *', min_value=0.0, key='BloodPressure')
+        BloodPressure = st.number_input('Blood Pressure Level *', min_value\
+                                        =0.0, key='BloodPressure')
         Age = st.number_input('Age *', min_value=16, key='Age')
     with col2:
         BMI = st.number_input('Body Mass Index *', min_value=0.0, key='BMI')
@@ -279,14 +116,17 @@ elif selection == 'Get Glucose Level':
         st.text("")
         st.text("")
         if st.button('Compute'):
-            glucose_estimate = int(gluco_dt.predict([[BloodPressure, BMI, Age]]))
+            glucose_estimate = int(gluco_dt.predict([[BloodPressure, BMI, \
+                                                      Age]]))
             
             st.success(glucose_estimate)
+# --------------------------Glucose Page----------------------------------
 
+# --------------------------BMI Page--------------------------------------
 # BMI Computation
 elif selection == 'BMI Calculation':
     st.markdown("""
-    <div style="background:#13202c ;padding:10px">
+    <div style="background:#000080 ;padding:10px">
     <h2 style="color:white;text-align:center;">Body Mass Index Computation</h2>
     </div>
     """, unsafe_allow_html=True)
@@ -303,18 +143,20 @@ elif selection == 'BMI Calculation':
     with col2:
         st.text("")
         st.success(bmi_value)
+# --------------------------BMI Page--------------------------------------
 
-# Diabetes Diagnosis
+# --------------------------Model Prediction------------------------------
 else:
     st.markdown("""
-    <div style="background:#13202c ;padding:10px">
+    <div style="background:#000080 ;padding:10px">
     <h2 style="color:white;text-align:center;">Welcome to AIDScanner! </h2>
     </div>
     """, unsafe_allow_html=True)
     col1, col2 = st.columns(2)
     with col1:
         Glucose = st.number_input('Glucose', min_value=0.0, key='Glucose')
-        BloodPressure = st.number_input('Blood Pressure Level *', min_value=0.0, key='BloodPressure')
+        BloodPressure = st.number_input('Blood Pressure Level *', \
+                                        min_value=0.0, key='BloodPressure')
     with col2:
         BMI = st.number_input('Body Mass Index *', min_value=0.0, key='BMI')
         Age = st.number_input('Age *', min_value=16, key='Age')
@@ -326,16 +168,22 @@ else:
             prediction = 'DIABETIC' if outcome[0] == 1 else 'NOT DIABETIC'
     with col2:
         st.success(prediction)
+# --------------------------Model Prediction------------------------------
 
-# Disclaimer
+# --------------------------Disclaimer------------------------------------
 st.markdown("""
-<div style="background:#13202c; padding:10px; margin-top:20px; border-top: 1px solid #ddd;">
+<div style="background:#000080; padding:10px; margin-top:20px; border-top: \
+    1px solid #ddd;">
 <h4 style="color:#fff;text-align:center;">Disclaimer</h4>
 <p style="color:#fff;text-align:center;">
-This application predicts the likelihood of diabetes based on the given parameters.
-However, it is not a substitute for professional medical advice, diagnosis, or treatment.
-Always seek the advice of your physician or other qualified health providers with any questions you may have regarding diabetes.
+This application predicts the likelihood of diabetes based on the given \
+parameters.
+However, it is not a substitute for professional medical advice, diagnosis, \
+or treatment.
+Always seek the advice of your physician or other qualified health providers \
+with any questions you may have regarding diabetes.
 </p>
 </div>
 """, unsafe_allow_html=True)
 
+# --------------------------Disclaimer------------------------------------
