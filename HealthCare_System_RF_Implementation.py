874f3899cd89310ea2689b1df47daf421b778b89
kesfranklyn25 <129005089+kesfranklyn25@users.noreply.github.com>
2024-07-25 20:12:04 +0100

    Update HealthCare_System_RF_Implementation.py

--- HealthCare_System_RF_Implementation.py
+++ HealthCare_System_RF_Implementation.py
@@ -40,8 +40,8 @@ gluco_dt = pickle.load(open('gluco_dt.sav', 'rb'))
 
 # --------------------------Sidebar Colour--------------------------------
 # --------------------------Hosted with Streamlit-------------------------
-# st.markdown("""
-#     <style>
+st.markdown("""
+    <style>
 #         a {
 #         color: #000080;
 #         text-decoration: inherit;
@@ -50,20 +50,20 @@ gluco_dt = pickle.load(open('gluco_dt.sav', 'rb'))
 #         text-decoration-style: ;
 #         text-decoration-color: ;
 #         }
-#         .viewerBadge_container__r5tak
-#         {
-#             # bottom: 0;
-#             visibility: hidden;
-#             display: none;
-#             # font-size: .875rem;
-#             # line-height: 1.25rem;
-#             # position: fixed;
-#             # right: 0;
-#             # z-index: 50;
-#         }
+        .viewerBadge_container__r5tak
+        {
+            # bottom: 0;
+            visibility: hidden !important;
+            display: none !important;
+            # font-size: .875rem;
+            # line-height: 1.25rem;
+            # position: fixed;
+            # right: 0;
+            # z-index: 50;
+        }
     
-#     </style>
-# """, unsafe_allow_html=True)
+    </style>
+""", unsafe_allow_html=True)
 
 hide_streamlit_style = """
             <style>
